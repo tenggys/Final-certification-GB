19638b2f1367f8df44e69cc85e10fd033158dc46
tenggys <levgood21@yandex.ru>
2023-12-02 08:09:59 +0300

    итоговая аттеставция

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BlogView from '../views/BlogView.vue'
+import BlogDetailsView from '../views/BlogDetailsView.vue'
+import ProjectView from '../views/ProjectView.vue'
+import ProjectDetailsView from '../views/ProjectDetailsView.vue'
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +16,30 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/blog',
+    name: 'blog',
+    component: BlogView
+  },
+  {
+    path: '/blogdetails',
+    name: 'blogdetails',
+    component: BlogDetailsView
+  },
+  {
+    path: '/project',
+    name: 'project',
+    component: ProjectView
+  },
+  {
+    path: '/projectdetails',
+    name: 'projectdetails',
+    component: ProjectDetailsView
+  },
+  {
+    path: '*',
+    name: '404',
+    component: NotFound
+  },
 ]
 
 const router = new VueRouter({
